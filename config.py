92761d53dbf0e03b36d0934486968b50539be823
meddling-moose <ryanhoffman@Ryans-MacBook-Pro-13.local>
2024-03-18 16:34:12 -0700

    made strides in returning album data with spotipy

---
+++ config.py
@@ -0,0 +1,3 @@
+username = 'ryan_hoffman-us'
+client_id = '25e72c8800394165a1af4116a118836f'
+client_secret = 'b3286ac633c841c4b676ce46e73c2434'
--- spotify-main.py
+++ spotify-main.py
@@ -6,6 +6,10 @@ import spotipy
 import pandas
 import spotipy.util as util
 from json.decoder import JSONDecodeError
+import requests
+#from config import username
+#from config import client_id
+#from config import client_secret
 
 #hardcode the values for now
 username = 'ryan_hoffman-us' #Change this to allow for username input
@@ -27,6 +31,29 @@ name = user['display_name'].split(' ')
 print('%s spotify account' %(str(name[0])))
 
 #Practice by retrieving all of the Talking Heads albums
-query_url = 'https://api.spotify.com/v1/artists/' #?
+talking_heads = '2x9SpqnPi8rlE9pjHBwmSC'
+anri_uri = 'spotify:artist:0xGtOrmB2hnrNRLG3vhpSo'
 
-#Retrieve the talking heads artist id
+#Retrieve the talking heads artist id, anri artist id
+#Anri - https://open.spotify.com/artist/0xGtOrmB2hnrNRLG3vhpSo?si=AqFjbL0fSGuahy7mZNQVww
+#Talking Heads - https://open.spotify.com/artist/2x9SpqnPi8rlE9pjHBwmSC?si=4I0U_j3sRmqmJ3FnugreZQ
+
+results = sp.artist_albums(anri_uri, album_type='album')
+anris_albums = results['items']
+
+while results['next']: #Stole this from stack overflow, but it's also in the spotipy get started page
+    results = sp.next(results)
+    anris_albums.extend(results['items'])
+
+print('-- Anris Spotify Album Data --')
+for album in anris_albums:
+    print('-~-~-')
+    print('Album Name: ' + album['name'])
+    print('Album ID: ' + album['id'])
+    print('Album Release Year: ' + album['release_date'])
+    print('Number of tracks: ' + str(album['total_tracks']))
+print('END OF LIST')
+
+sp.artist(anri_uri)
+timely = sp.album('spotify:album:2X9EFWmSWAYLjKm9y6llqL') # https://open.spotify.com/album/2X9EFWmSWAYLjKm9y6llqL?si=YXWQpU8LQRSVg4hCcsnj6Q
+#print(timely)
